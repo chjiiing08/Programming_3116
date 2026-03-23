d0d294c74190c359cdfe3ce451f2c22574c966a3
chjiiing08 <s2451@gmail.com>
2026-03-23 14:15:24 +0900

    counter JS onclick()

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -3,13 +3,11 @@ const pButton = document.getElementById("pb");
 
 let cnt = 0;
 
-pButton.addEventListener("click",() => {
-    cnt++;
-    num.innerHTML = cnt;
-});
-
-// const mButton = document.getElementById("mb");
-// mButton.addEventListener("click",() => {
-//     cnt--;
+// pButton.addEventListener("click",() => {
+//     cnt++;
 //     num.innerHTML = cnt;
 // });
+pButton.onclick = () => {
+    cnt++;
+    num.innerHTML = cnt;
+};
