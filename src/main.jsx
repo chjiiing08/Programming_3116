3035af8b2a4d291d3c408927cc0b3c529cfd689a
chjiiing08 <s2451@gmail.com>
2026-04-06 14:18:30 +0900

    TodoListApp 시작 ( •̀ ω •́ )✧

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import CounterApp from './CounterApp'
+import TodoListApp from './TodoListApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <TodoListApp />
   </StrictMode>,
 )
