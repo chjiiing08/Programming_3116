48e05ed873d2c9174801a123195ef0e1a8d012e5
조현진 <s2451@e-mirim.hs.kr>
2026-04-13 14:26:22 +0900

    TodoListApp h1 -> TodoHeader, li -> TOdoItemEmpty, input,label -> CheckBox

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,18 +1,21 @@
 import './todolist.css'
 import Button from './components/Button.jsx';
+import TodoItemEmpty from './components/TodoItemEmpty.jsx';
+import TodoHeader from './components/TodoHeader.jsx';
+import CheckBox from './components/CheckBox.jsx';
 
 function TodoListApp(){
     return(
         <div className="todo">
-            <h1 className='todo__title'>ToDo List</h1>
+            <TodoHeader/>
             <form className='todo__form'>
                 <input type="text"placeholder="할 일을 입력하세요" className='todo__input'/>
                 <Button type='submit' className='todo__button todo__button--add'>Add</Button>
             </form>
             <ul className='todo__list'>
+                <TodoItemEmpty/>
                 <li className='todo__item todo__item--complete'>
-                    <input type="checkbox" id="chk-1" className='todo__check'/>
-                    <label htmlFor="chk-1" className='todo__label'>놀기</label>
+                   <CheckBox id="1">할 일1</CheckBox>
                     <Button className='todo__button todo__button--edit'>✏️</Button>
                     <Button className='todo__button todo__button--delete'>❌</Button>
                 </li>
---
+++ src/components/CheckBox.jsx
@@ -0,0 +1,9 @@
+export default function CheckBox(props) {
+    const {children, id, ...rest} = props;
+    return (
+        <>
+            <input type="checkbox" id={`chk-${id}`} className='todo__check' />
+            <label htmlFor={`chk-${id}`} className='todo__label'>{children}</label>
+        </>
+    )
+}
