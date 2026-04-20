83405d6cc6522eb40bb3f40e0d4d8808f6539c9a
조현진 <s2451@e-mirim.hs.kr>
2026-04-20 13:10:15 +0900

    TodoListApp addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './todolist.css'
 import TodoHeader from './components/TodoHeader.jsx';
 import TodoAdder from './components/TodoAdder.jsx'
 import TodoList from './components/TodoList.jsx';
 
+class Todo{
+    constructor(text){
+        this.id = Date.now();       // 할일 고유 id: 만든 시각, newDate().getItem
+        this.text = text;           // 할일 내용
+        this.isCompleted = false;   //완료 여부: 기본값 false
+    }
+}
+
 function TodoListApp(){
+    const [todos, setTodos] = useState([]); //할일 목록: 기본값 빈 리스트 
+    const addTodo = (text) => setTodos((todos) => [
+        // 이전 todos 복사
+        ...todos,
+        // newTodo 만들자
+        // 이전 todos 추가하자
+        new Todo(text)
+    ]);
+    
     return(
         <div className="todo">
             <TodoHeader/>
-            <TodoAdder/>
+            <TodoAdder addTodo={addTodo}/>
             <TodoList/>
         </div>
     )
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import Button from './Button.jsx';
 
-export default function TodoAdder() {
+export default function TodoAdder({ addTodo }) {
+    const [inputTodo, setInputTodo] = useState('');
+    const handleSubmit = (event) =>{
+        event.preventDefault(); //submit의 기본 동작 막자
+        //빈칸이면 바로 return
+        if (!inputTodo) return;
+
+        //inputTodo에서 사용자가 입력한 text 가져오자
+        //addTodo에 그 text 넣자
+        addTodo(inputTodo.trim());
+        setInputTodo('');   //input 빈칸 만들자
+
+    }
     return(
-        <form className='todo__form'>
-            <input type="text" placeholder="할 일을 입력하세요" className='todo__input' />
+        <form className='todo__form' onSubmit={handleSubmit}>
+            <input type="text" placeholder="할 일을 입력하세요" className='todo__input' value={inputTodo} onChange={(event) => setInputTodo(event.target.value)} />
             <Button type='submit' className='todo__button todo__button--add'>Add</Button>
         </form>
     )
